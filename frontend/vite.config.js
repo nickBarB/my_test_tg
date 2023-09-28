3fc14062ea659f2466787087d76b6db967320403
nick <nik.kanygin@icloud.com>
2023-09-28 11:02:56 +0500

    ++

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const cssFileName = 'index.min.css'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: './public',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (file) => {
-          return `assets/css/${cssFileName}`
-        },
-        entryFileNames: (file) => {
-          return `assets/js/[name].min.js`
-        },
-      }
-    }
-  }
 })
